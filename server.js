a9f5b8f20672a79c8abfd4723f812a09531b3285
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2022-11-27 19:56:38 -0700

    installed all necessary packages and started creating routes in server.js

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+const noteData = require("./db/db.json");
+const fs = require("fs");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// allows express to serve static files from the 'public' directory.
+app.use(express.static("public"));
+
+// creates a GET route to index.html.
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+// creates a GET route to notes.html.
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+// creates a GET route to read and return all saved notes from the 'db.json' file.
+app.get("/api/notes", (req, res) => {
+    res.json(noteData);
+});
+
+// creates a POST route to receive a new note to save on the request body, add it to the 'db.json' file, and then return the new note to the client. Gives each note a unique ID when its saved. (look up NPM packages to do this).
+
+// instantiates a PORT
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
+});
