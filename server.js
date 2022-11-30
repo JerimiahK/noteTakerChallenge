ef07bed12c2339794fcb8981c144feae32b5ce9d
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2022-11-29 17:19:05 -0700

    got the post and delete functions working

--- server.js
+++ server.js
@@ -26,29 +26,39 @@ app.get("/notes", (req, res) => {
 
 // creates a GET route to read and return all saved notes from the 'db.json' file.
 app.get("/api/notes/", (req, res) => {
-  return res.json(noteData);
+  res.json(noteData);
 });
 
 // creates a POST route to receive a new note to save on the request body, add it to the 'db.json' file, and then return the new note to the client. Gives each note a unique ID when its saved. (look up NPM packages to do this).
 app.post("/api/notes", (req, res) => {
   console.log(`${req.method} request received`);
-  let notesArr = fs.readFileSync("./db/db.json");
-  console.log(notesArr + "notesArr");
-  notesArr = JSON.parse(notesArr);
-  res.json(notesArr);
-  let newNote = {
-    title: req.body.title,
-    text: req.body.text,
-    id: uniqid(),
-  };
-  // notesArr.push(newNote);
-  //new variable = [...notesArr, newNote]
-  let pushNote = [...notesArr, newNote];
-  fs.writeFileSync("./db/db.json", JSON.stringify(pushNote));
-  console.log(newNote + "newNote");
+  const { title, text } = req.body;
+
+  if (text && title) {
+    let newNote = {
+      title,
+      text,
+      id: uniqid(),
+    };
+    noteData.push(newNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(noteData, null, 4));
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+    res.json(response);
+  }
 });
 
-app.delete("/api/notes", (req, res) => {});
+app.delete("/api/notes/:id", (req, res) => {
+  const { id } = req.params;
+  const noteIndex = noteData.findIndex((note) => note.id === id);
+  console.log(noteIndex);
+  let removeNote = noteData.splice(noteIndex, 1);
+  fs.writeFileSync("./db/db.json", JSON.stringify(removeNote, null, 4));
+  return res.send(removeNote);
+});
 
 // instantiates a PORT
 app.listen(PORT, () => {
